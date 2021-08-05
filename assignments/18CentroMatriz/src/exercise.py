7e3e2d0b2e87e9cd7161b5212332fc121cb76af0
Jorge Mario Cruz-Duarte <jorge.cruz@itesm.mx>
2021-08-05 18:26:28 -0500

    No answers

--- assignments/18CentroMatriz/src/exercise.py
+++ assignments/18CentroMatriz/src/exercise.py
@@ -1,29 +1,6 @@
-def generaMatriz (renglon, columna):
-    matriz = []
-    for i in range (renglon):
-        lista = []
-        for y in range(columna):
-            valor = int(input())
-            lista.append(valor)
-        matriz.append(lista)
-    return matriz
-
-def centroMatriz(matriz):
-    matrizSalida=[]
-    for ren in range(len(matriz)-2):
-        vector=[]
-        for col  in range(len(matriz[0])-2):
-            vector.append(matriz[ren+1][col+1])       
-        matrizSalida.append(vector)        
-    return matrizSalida
-
 def main():
-    renglon = int(input())
-    columna = int(input())
-    matriz = generaMatriz (renglon, columna)
-    matriz=centroMatriz(matriz)
-    print(matriz)
+    #escribe tu código abajo de esta línea
+    pass
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
